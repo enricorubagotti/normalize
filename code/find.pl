b152563c507997079dc4305fcce15643d4340809
enricorubagotti <66751509+enricorubagotti@users.noreply.github.com>
2022-09-23 16:11:49 -0500

    Add files via upload

--- code/find.pl
+++ code/find.pl
@@ -1,9 +1,10 @@
 use strict;
 use List::Util qw(min);
 #use List::UtilsBy; 
-my $input="../depa.bwt.sorted";
-
+binmode STDOUT , ":utf8";
+my $depa_bwt="../depa.bwt.sorted";
 my $depa="../depa";
+my $muni="../muni.csv";
 my %depa; 	# $depa{"depa name"}=sum of the letters equals between the 
 			# query and the match.
 			# It is used when there is not an exact coincidence between query and substring
@@ -15,9 +16,13 @@ my %depa; 	# $depa{"depa name"}=sum of the letters equals between the
 			#IOQUIA^IANTIOQUIA$
 			#IOQUI
 			#IPIÉLAGO DE SAN ANDRÉS, PROVIDENCIA Y SANTA CA^IARCHIPIÉLAGO DE SAN ANDRÉS, PROVIDENCIA Y SANTA CA$			
+my %muni
+my $depa_Muni="../depa_Muni.csv";
+my %depa_Muni;
 my $bestDepaTruncated="NULL";
 my $bestDepaTruncatedScore=0;
-my $query=uc($ARGV[0]);
+#my $query=uc($ARGV[0]);
+my $file2Correct=$ARGV[0];
 my $maxLeven=$ARGV[1];
 my @circularDic0;
 my @circularDic1;
@@ -43,8 +48,14 @@ sub leven {
       };
 }
 
-sub find {
+
+
+
+sub find_binary {
 	    my ($array_ref, $value, $left, $right) = @_;
+	    
+
+	    
 #	    print "query = $value left= $left right=$right \n";
 	        return ($left."\t".$right) if ($right < $left);
 	        #Updating the score of the Department
@@ -74,9 +85,6 @@ sub find {
 				
 				}
 
-
-
-
 		#compare left and right with the $value char by char
 		#print "I am returning ".$left." ".$right.",  $array_ref->[$left] \t $array_ref->[$right]\n";
 		    my $middle = int(($right + $left) >> 1);
@@ -87,49 +95,29 @@ sub find {
 					    }
 					        elsif ($compare < 0 ) {
 			#				print "I am seeking in the upper part of the array between $array_ref->[$left] and  $array_ref->[$middle]\n";
-							        find($array_ref, $value, $left, $middle - 1);
+							        find_binary($array_ref, $value, $left, $middle - 1);
 								    }
 								        else {
 			#								print "I am seeking in the lower part of the array between $array_ref->[$middle] and $array_ref->[$right] \n";
-										        find($array_ref, $value, $middle + 1, $right);
+										        find_binary($array_ref, $value, $middle + 1, $right);
 											    }
 										    }
-										    
-
-
-binmode STDOUT	, ":utf8";
-open(input,"<$input") or print $input." does not exist\n";
-binmode input, ":utf8";
-my $lCounter=0;
-while (my $line=<input>)
-	{
-	$line=~s/\n|\r//g;
-	my @line=split(/\t/,$line);
-	$circularDic0[$lCounter]=$line[0];
-	$circularDic1[$lCounter]=$line[1];
-	$lCounter++;
-	}
-	close input;
-
-open(depa,"<$depa") or print $depa."does not exist\n";
-binmode depa, ":utf8";
-	while (my $line=<input>)
-        {
-	$line=~s/\n|\r//g;
-	$depa{$line}=0;
-	}	
+###################################################################
+sub return_Match{
+	my $query=$_[0];
 	my $longestLength=0;
 	my $longestMatch="Null";
 	my $longestOriginalWord;
+	#SET TO 0 THE FREQUENCY HASHES
 	if (!exists $depa{$query})
 		{
 		for (my $c=0;$c<length($query);$c++)
 			{
-			my $directBoundary=find(\@circularDic0,substr($query,0,$c+1),0,scalar(@circularDic0)-1);
+			my $directBoundary=find_binary(\@circularDic0,substr($query,0,$c+1),0,scalar(@circularDic0)-1);
 			my @returnDirect=split(/\t/,$directBoundary);
 			if (($returnDirect[1] eq "Found") and (length($returnDirect[1]) > $longestLength))
 				{
-				print "new longest match $longestMatch\n";
+				#print "new longest match $longestMatch\n";
 				$longestMatch=$returnDirect[0];
 				$longestLength=length($returnDirect[0]);
 				$longestOriginalWord=$circularDic0[$returnDirect[0]];
@@ -139,7 +127,7 @@ binmode depa, ":utf8";
 				{
 				#print "foundQuery between ".$circularDic0[$returnDirect[0]]."\t".$circularDic0[$returnDirect[1]]."\n";
 				}
-				my $reverseBoundary=find(\@circularDic0,substr($query,$c,length($query)-$c),0,scalar(@circularDic0)-1);
+				my $reverseBoundary=find_binary(\@circularDic0,substr($query,$c,length($query)-$c),0,scalar(@circularDic0)-1);
 				my @returnReverse=split(/\t/,$reverseBoundary);
 			if (($returnReverse[1] eq "Found") and (length($returnReverse[1]) > $longestLength))
 				{
@@ -153,24 +141,164 @@ binmode depa, ":utf8";
 				{
 				#print "foundQuery between ".$circularDic0[$returnReverse[0]]."\t".$circularDic0[$returnReverse[1]]."\n";
 				}
-				#print $circularDic1[find(\@circularDic0,substr($query,0,$c+1),0,scalar(@circularDic0))]."\n";			
-				#print $circularDic1[find(\@circularDic0,substr($query,$c,length($query)-$c),0,scalar(@circularDic0))]."\n";
+				#print $circularDic1[find_binary(\@circularDic0,substr($query,0,$c+1),0,scalar(@circularDic0))]."\n";			
+				#print $circularDic1[find_binary(\@circularDic0,substr($query,$c,length($query)-$c),0,scalar(@circularDic0))]."\n";
 			}
 		}
 	
 #Check output using levenDistance
 if (leven($query,$longestOriginalWord)<$maxLeven+1)
 	{
-	print "$query corrected is ".$longestOriginalWord."\n";
+	return $longestOriginalWord;
 	}
 else
 	{
 	if (leven($query,$bestDepaTruncated)<$maxLeven+1)
 		{
-		print "\nThe best match is $bestDepaTruncated\n";
+		return $bestDepaTruncated;
 		}
 	else 
 		{
-		print "I could not find a match for $query\n";	
+		return "-1";	
 		}
 	}
+}
+
+sub normalize_csv {
+	#initialize to 0 the frequency hashes;
+	
+	foreach my $key (keys %depa)
+		{
+		$depa{$key}=0;	
+		}
+
+	foreach my $key (keys %muni)
+		{
+		$muni{$key}=0;	
+		}
+		
+	foreach my $key (keys %depa_Muni)
+		{
+		$depa_Muni{$key}=0;	
+		}
+	}
+sub column_muni{
+	#it opens the file
+	}
+	
+sub returnSeparator {
+	my $fileName=$_[0];
+	my @freq_char;# this is an array of hashes, doc at https://www.educba.com/perl-array-of-hashes/
+	open(file,"<".$fileName) or print "$fileName.does not exist";
+	while(my $line=<file>)
+		{
+		$line=~s/\n/\r/g
+		for (my $c=0;$c<length($line);$c++)
+				{
+				if (exists $freq_char[$lineN]{substr($line,$c,1)})
+						{
+						$freq_char[$lineN]{substr($line,$c,1)}++;
+						}
+				else
+						{
+						$freq_char{substr($line,$c,1)}=1;
+						}
+				}
+		}
+	}
+
+open(depa_bwt,"<$depa_bwt") or print $depa_bwt." does not exist\n";
+binmode depa_bwt, ":utf8";
+my $lCounter=0;
+while (my $line=<depa_bwt>)
+	{
+	$line=~s/\n|\r//g;
+	my @line=split(/\t/,$line);
+	$circularDic0[$lCounter]=$line[0];
+	$circularDic1[$lCounter]=$line[1];
+	$lCounter++;
+	}
+	close depa_bwt;
+
+open(depa,"<$depa") or print $depa."does not exist\n";
+binmode depa, ":utf8";
+while (my $line=<depa_bwt>)
+    {
+	$line=~s/\n|\r//g;
+	$depa{$line}=0;
+	}	
+
+open(muni,"<$muni") or print $muni." does not exist\n";
+while (my $line=<muni>)
+	{
+	$line=~s/\n|\r//g;
+	$muni{$line}=0;	
+	}
+close muni;
+
+
+while (my $line=<muni>)
+	{
+	$line=~s/\n|\r//g;
+	$muni{$line}=0;	
+	}
+open(depa_muni,"<".$depa_Muni) or print $depa_muni." does not exist\n"; #this file is ";" separated
+while (my $line=<depa_muni>)
+	{
+	$line				=~s/\n|\r//g;
+	$depa_Muni{$line}	=			1;
+	}
+
+#open(file,"<$file2Correct") or print "$file2Correct does not exist\n";
+#Which column is Depa(if there is one) and which column is Muni(if there is one)
+
+__END__
+=encoding UTF8
+
+=head1 DESCRIPTION
+	This script automatically spell check csv files. It was designed to automatically correct geographical names in the file.
+	Input: a CSV file
+	Output: a CSV file with the last three column: Department(state), 
+	City(Municipio),id
+	or, when we do not have the department Municipio, ID, or when we do not have the City Department(ID)
+	For debugging porpuses it is a series of nested functions that 
+	are  executed as normalize_csv(column_depa,column_muni,return_Match(find_binary(leven())))
+
+=head2 normalize_csv(fileName)
+
+	This is the more external function.
+	Input: csv file name
+	Output: the csv file name + corrected geographical location
+	THIS IS TO FINISH!!!!
+
+=head2 return_Match(query,\array)
+
+	Given a string(query) and an array it returns the nearest word  
+
+=head2 binary($array_ref, $value, $left, $right)
+	The function find_binary  return the best match in an array to an 
+	alphanumerical  query. 
+	Input:reference to an array, query, minimum index  in the array, 
+	maximum index  in the array.
+	The code is recursive and for this reason it uses min and max value
+
+=head2 leven($a,$b)
+
+	This function calculates the levenstain distance between two 
+	strings($a and $b)
+
+=head2 column_depa(file_csv,\array_depa)
+	This function returns the column number (first column=column0)
+	of the department in the initial file. It returns NULL if 
+	cannot locate any columns
+	
+=head2 column_muni(file_csv,\array_municipios)
+	This function returns the column number (first column=column0)
+	of the municipality in the initial file
+	It returns NULL if it cannot locate any columns
+= head2 returnSeparator(file_name)
+	It returns the separator of a csv file.
+	It strips the EOL and count the frequency of chars in 10 lines.
+	It returns the separator as the char with identical frequency 
+	in all 10 lines 
+=cut
